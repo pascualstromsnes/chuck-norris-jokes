244eda83be9614bd2f4877a756b9a7b55040cae4
Pascual Strømsnæs <pascualstromsnaes@MacBook-Pro-138.lan>
2019-11-17 00:37:01 +0100

    Initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,27 @@
+<?php
+namespace Pascualstromsnes\ChuckNorrisJokes;
+
+class JokeFactory
+{
+    protected $jokes = [
+        'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
+        'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+        'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+        'Chuck Norris counted to infinity... Twice.',
+        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
+        'When the Boogeyman goes to sleep at night he checks his closet for Chuck Norris.',
+        
+    ];
+    
+    public function __construct(array $jokes = null)
+    {
+        if($jokes){
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Pascualstromsnes\ChuckNorrisJokes\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Pascualstromsnes\ChuckNorrisJokes\JokeFactory;
+
+class JokeFactoryTest extends TestCase
+{
+
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+     /** @test */
+     public function it_returns_a_predefined_joke()
+     {
+        $chuckNorrisJokes = [
+            'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
+            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+            'Chuck Norris counted to infinity... Twice.',
+            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
+            'When the Boogeyman goes to sleep at night he checks his closet for Chuck Norris.',
+            
+        ]; 
+        
+        $jokes = new JokeFactory();
+ 
+         $joke = $jokes->getRandomJoke();
+ 
+         $this->assertContains($joke, $chuckNorrisJokes);
+     }
+
+}
