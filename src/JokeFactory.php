79073df35a96f2fafc75a4b3645ff60dc6224f85
Pascual Strømsnæs <pascual.stromsnes@gmail.com>
2019-11-17 12:42:35 +0100

    Switch to using joke API

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -2,27 +2,26 @@
 
 namespace Pascualstromsnes\ChuckNorrisJokes;
 
+use GuzzleHttp\Client;
+
 class JokeFactory
 {
-    protected $jokes = [
-        'The First rule of Chuck Norris is that you do not talk about Chuck Norris.',
-        'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
-        'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-        'Chuck Norris counted to infinity... Twice.',
-        'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
-        'When the Boogeyman goes to sleep at night he checks his closet for Chuck Norris.',
-
-    ];
-
-    public function __construct(array $jokes = null)
+    const API_ENDPOINT = 'http://api.icndb.com/jokes/random';
+    
+    protected $client;
+    
+    public function __construct(Client $client = null)
     {
-        if ($jokes) {
-            $this->jokes = $jokes;
-        }
+        $this->client = $client ?: new Client();
     }
 
     public function getRandomJoke()
     {
-        return $this->jokes[array_rand($this->jokes)];
+        $response = $this->client->get(self::API_ENDPOINT);
+
+        $joke = json_decode($response->getBody()->getContents());
+
+        return $joke->value->joke;
     }
 }
+ 
--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -2,40 +2,31 @@
 
 namespace Pascualstromsnes\ChuckNorrisJokes\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Exception\RequestException;
+use PHPUnit\Framework\TestCase; 
 use Pascualstromsnes\ChuckNorrisJokes\JokeFactory;
-use PHPUnit\Framework\TestCase;
 
 class JokeFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_random_joke()
     {
-        $jokes = new JokeFactory([
-            'This is a joke',
+        $mock = new MockHandler([
+            new Response(200, [], '{ "type": "success", "value": { "id": 476, "joke": "Chuck Norris doesn\'t need a debugger, he just stares down the bug until the code confesses.", "categories": ["nerdy"] } }'), 
         ]);
 
-        $joke = $jokes->getRandomJoke();
+        $handler = HandlerStack::create($mock);
 
-        $this->assertSame('This is a joke', $joke);
-    }
-
-    /** @test */
-    public function it_returns_a_predefined_joke()
-    {
-        $chuckNorrisJokes = [
-            'The First rule of Chuck Norris is: you do not talk about Chuck Norris.',
-            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
-            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-            'Chuck Norris counted to infinity... Twice.',
-            'If you can see Chuck Norris, he can see you. If you can\'t see Chuck Norris you may be only seconds away from death.',
-            'When the Boogeyman goes to sleep at night he checks his closet for Chuck Norris.',
-
-        ];
-
-        $jokes = new JokeFactory();
+        $client = new Client(['handler' => $handler]);
+        
+        $jokes = new JokeFactory($client);
 
         $joke = $jokes->getRandomJoke();
 
-        $this->assertContains($joke, $chuckNorrisJokes);
+        $this->assertSame('Chuck Norris doesn\'t need a debugger, he just stares down the bug until the code confesses.', $joke);
     }
 }
