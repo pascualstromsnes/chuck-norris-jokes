5d02ba40ec896a69286fd1fc2c52a90efad49b19
pascualstromsnes <pascual.stromsnes@gmail.com>
2019-11-17 11:42:49 +0000

    Apply fixes from StyleCI

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -7,9 +7,9 @@ use GuzzleHttp\Client;
 class JokeFactory
 {
     const API_ENDPOINT = 'http://api.icndb.com/jokes/random';
-    
+
     protected $client;
-    
+
     public function __construct(Client $client = null)
     {
         $this->client = $client ?: new Client();
@@ -24,4 +24,3 @@ class JokeFactory
         return $joke->value->joke;
     }
 }
- 
