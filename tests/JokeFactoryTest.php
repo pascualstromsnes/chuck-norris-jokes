8b730c5868164f9c5ccc1f21aefacadfac866ac4
Pascual Strømsnæs <pascual.stromsnes@gmail.com>
2019-11-17 12:42:57 +0100

    Apply fixes from StyleCI (#2)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -3,12 +3,11 @@
 namespace Pascualstromsnes\ChuckNorrisJokes\Tests;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\Exception\RequestException;
-use PHPUnit\Framework\TestCase; 
 use Pascualstromsnes\ChuckNorrisJokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
 
 class JokeFactoryTest extends TestCase
 {
@@ -16,13 +15,13 @@ class JokeFactoryTest extends TestCase
     public function it_returns_a_random_joke()
     {
         $mock = new MockHandler([
-            new Response(200, [], '{ "type": "success", "value": { "id": 476, "joke": "Chuck Norris doesn\'t need a debugger, he just stares down the bug until the code confesses.", "categories": ["nerdy"] } }'), 
+            new Response(200, [], '{ "type": "success", "value": { "id": 476, "joke": "Chuck Norris doesn\'t need a debugger, he just stares down the bug until the code confesses.", "categories": ["nerdy"] } }'),
         ]);
 
         $handler = HandlerStack::create($mock);
 
         $client = new Client(['handler' => $handler]);
-        
+
         $jokes = new JokeFactory($client);
 
         $joke = $jokes->getRandomJoke();
